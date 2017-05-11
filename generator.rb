ce0dc17589daafc18eccafc887f96e2f9297e093
David Bowers <dbowers.github@gmail.com>
2017-05-11 00:26:14 -0600

    simplify generator class

--- generator.rb
+++ generator.rb
@@ -1,10 +1,7 @@
 class Generator
   def self.words_for(category, options)
-    self.class_eval do
-      define_method(category) do
-        options.sample
-      end
-
+    define_method(category) do
+      options.sample
     end
   end
 end
